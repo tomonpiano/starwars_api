aa080940c8b8d9ab50c3c8e60ddce569bb47867c
Thomas Scott <tom@tomscottonline.com>
2018-06-23 08:05:55 +0100

    added button list

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      characters : [],
+      people : [],
       url : "https://swapi.co/api/people/",
     }
   }
@@ -13,17 +13,36 @@ class App extends Component {
   componentDidMount () {
     fetch(this.state.url)
     .then(response => response.json())
-    .then(response => this.setState({characters : response.results}))
+    .then(response => this.setState({people : response.results}))
+  }
+
+  createListButtons (list) {
+    const buttonList = list.map((element, i) => {
+      return (
+      <div key={i}>
+        <button onClick={() => this.handleClick(i)}>
+          {element.name}
+        </button>
+      </div>
+      )
+    });
+    return (
+      buttonList
+    )
+  }
+
+  handleClick (i) {
+    return console.log("clicked: " + this.state.people[i].name)
   }
 
   render() {
-    return !this.state.characters.length ?
+    return !this.state.people.length ?
         <div>Loading</div> :
         (
-          <div className="App">
-            <header className="App-header">
-              {this.state.characters[0].name}
-            </header>
+          <div>
+            <h1>
+              {this.createListButtons(this.state.people)}
+            </h1>
           </div>
         )
   }
